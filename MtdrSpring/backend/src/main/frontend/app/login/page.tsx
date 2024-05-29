0d9558ad66d50b3a84343e3073a8cff077718d7a
Juan Zambrano <jpablozambrano@hotmail.com>
2024-05-29 16:26:31 -0600

    Update page.tsx
    Fixed log in

--- MtdrSpring/backend/src/main/frontend/app/login/page.tsx
+++ MtdrSpring/backend/src/main/frontend/app/login/page.tsx
@@ -13,7 +13,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const response = await axios.get(`https://team12.kenscourses.com/${email}/${password}`);
+      const response = await axios.get(`https://team12.kenscourses.com/users/${email}/${encodeURIComponent(password)}`);
       const user = response.data;
       console.log("User:", user);
 
