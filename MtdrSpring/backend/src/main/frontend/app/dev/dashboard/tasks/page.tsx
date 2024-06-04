b9dc0437c4f69bb4cccdb28d3f60a6ab0a104753
Stefano H.A. <stefano.herrejon@hotmail.com>
2024-06-04 16:04:06 -0600

    Otro boton

--- MtdrSpring/backend/src/main/frontend/app/dev/dashboard/tasks/page.tsx
+++ MtdrSpring/backend/src/main/frontend/app/dev/dashboard/tasks/page.tsx
@@ -13,7 +13,6 @@ import Image from "next/image";
 import r from "../../../responsive.module.css";
 import s from "./tasks.module.css";
 import Link from "next/link";
-import axios from "axios";
 
 export default function Home() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -28,10 +27,9 @@ export default function Home() {
   /* CURRENT USER */
   const [user, setUser] = useState<{
     id: number;
-    email: string;
     name: string;
-    team_id: number;
-    role: string;
+    developer_id: number;
+    manager_id: number;
   }>();
 
   useEffect(() => {
@@ -43,61 +41,28 @@ export default function Home() {
 
   /* TASK LIST */
   const [tasks, setTasks] = useState<
-  {
-    id: number;
-    title: string;
-    description: string;
-    priority: string;
-    status: string;
-    developer_id: number; // Change the type to number
-    completed_at: string;
-    created_at: string;
-    updated_at: string;
-    notes: string;
-    developer: {
+    {
       id: number;
-      email: string;
-      team_id: number;
-      role: string;
-      name: string;
-    };
-    team: {
-      id: number;
-      name: string;
-    };
-  }[]
+      title: string;
+      priority: string;
+      status: string;
+      developer_id: number;
+    }[]
   >([]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedTasksString = localStorage.getItem(
+        "team_" + user?.developer_id
+      );
+      setTasks(JSON.parse(storedTasksString || "[]"));
+    }
+  }, [user]);
 
   /* CURRENT TASK */
   const updateTask = (updatedTask: number) => {
     localStorage.setItem("currentTask", JSON.stringify(updatedTask));
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (typeof window !== 'undefined') {
-          const storedUser = localStorage.getItem('user');
-          if (storedUser) {
-            const user = JSON.parse(storedUser);
-            const userID = user.id;
-  
-            const response = await axios.get(`https://team12.kenscourses.com/tasks/developer/${userID}`);
-            const data = response.data;
-  
-            // Set tasks data
-            setTasks(data);
-            console.log("Data fetched for Active/History:", data);
-            console.log("User stored:", user);
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-  
-    fetchData();
-  }, []);
 
   return (
     <div
@@ -114,7 +79,7 @@ export default function Home() {
 
       <div /* Top Wrapper */ className={`${r.wrapper} ${s.titleFlex}`}>
         <div className={`${s.topTitle} font-bold`}>Welcome {user?.name}!</div>{" "}
-        <div className={` text-gray-600 `}>#Team {user?.team_id}</div>
+        <div className={` text-gray-600 `}>#Team {user?.developer_id}</div>
       </div>
       <div className={s.sFont}>Developer</div>
 
@@ -145,88 +110,96 @@ export default function Home() {
         }
       >
         <div className={s.mainBody}>
-        {active
-          ? // Render tasks that are not cancelled and have completed_at === null when active is true
-            tasks
-              .filter(
-                (task: { status: string; completed_at: string | null }) =>
-                  task.status !== "cancelled" && task.completed_at === null
-              )
-              .map(
-                (task: {
-                  id: number;
-                  title: string;
-                  priority: string;
-                  status: string;
-                }) => (
-                  <div key={task.id}>
-                    <div className={s.task}>
-                      <div>{task.title}</div>
-                      <div className={s.rightOpt}>
-                        <div
-                          className={
-                            task.priority === "low"
-                              ? `${s.priorityIndicator} bg-green-500`
-                              : task.priority === "medium"
-                              ? `${s.priorityIndicator} bg-yellow-500`
-                              : `${s.priorityIndicator} bg-red-500`
-                          }
-                        />
-                        <Link
-                          onClick={() => updateTask(task.id)}
-                          href="task/edit"
-                        >
-                          <img className={s.icon} src="/icons/edit.png" />
-                        </Link>
-                        <Link
-                          onClick={() => updateTask(task.id)}
-                          href="task/details"
-                        >
-                          <img className={s.icon} src="/icons/open.png" />
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
+          {active
+            ? // Render tasks that are not cancelled or completed when active is true
+              tasks
+                .filter(
+                  (task: { status: string; developer_id: number }) =>
+                    task.status !== "cancelled" &&
+                    task.status !== "completed" &&
+                    task.developer_id == user?.id
                 )
-              )
-          : // Render tasks that are cancelled or have completed_at !== null when active is false
-            tasks
-              .filter(
-                (task: { status: string; completed_at: string | null }) =>
-                  task.status === "cancelled" || task.completed_at !== null
-              )
-              .map(
-                (task: {
-                  id: number;
-                  title: string;
-                  priority: string;
-                  status: string;
-                }) => (
-                  <div key={task.id}>
-                    <div className={s.task}>
-                      <div>{task.title}</div>
-                      <div className={s.rightOpt}>
-                        <div
-                          className={
-                            task.status === "completed"
-                              ? `${s.status} bg-green-500`
-                              : `${s.status} bg-red-500`
-                          }
-                        >
-                          {task.status}
+                .map(
+                  (task: {
+                    id: number;
+                    title: string;
+                    priority: string;
+                    status: string;
+                  }) => (
+                    <div key={task.id}>
+                      <div className={s.task}>
+                        <div>{task.title}</div>
+                        <div className={s.rightOpt}>
+                          <div
+                            className={
+                              task.priority === "Low"
+                                ? `${s.priorityIndicator} bg-green-500`
+                                : task.priority === "Medium"
+                                ? `${s.priorityIndicator} bg-yellow-500`
+                                : `${s.priorityIndicator} bg-red-500`
+                            }
+                          />
+                          <Link
+                            onClick={() => updateTask(task.id)}
+                            href="task/edit"
+                          >
+                            <img className={s.icon} src="/icons/edit.png" />
+                          </Link>
+                          <Link
+                            onClick={() => updateTask(task.id)}
+                            href="task/details"
+                          >
+                            <img className={s.icon} src="/icons/open.png" />
+                          </Link>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  )
                 )
-              )}
+            : // Render tasks that are cancelled or completed when active is false
+              tasks
+                .filter(
+                  (task: { status: string; developer_id: number }) =>
+                    task.status === "cancelled" ||
+                    (task.status === "completed" &&
+                      task.developer_id === user?.id)
+                )
+                .map(
+                  (task: {
+                    id: number;
+                    title: string;
+                    priority: string;
+                    status: string;
+                  }) => (
+                    <div key={task.id}>
+                      <div className={s.task}>
+                        <div>{task.title}</div>
+                        <div className={s.rightOpt}>
+                          <div
+                            className={
+                              task.status === "completed"
+                                ? `${s.status} bg-green-500`
+                                : `${s.status} bg-red-500`
+                            }
+                          >
+                            {task.status}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                )}
         </div>
       </div>
 
       <div
         /* Bottom Wrapper */
         className={isBottom ? `${r.wrapper} ${r.bottom}` : r.wrapper}
-      ></div>
+      >
+        <Link href="/dev/dashboard" className={`${s.btn}  !bg-black`}>
+          Dashboard
+        </Link>
+      </div>
     </div>
   );
 }
