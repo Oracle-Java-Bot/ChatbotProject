d4080312f521e4024610e39f76b156af2b7c4dc0
flioner <lionerrochae@gmail.com>
2024-06-05 17:10:31 -0600

    performance review initial commit

---
+++ MtdrSpring/backend/src/main/frontend/app/manager/dashboard/performance/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+import {
+  AwaitedReactNode,
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  useEffect,
+  useState,
+} from "react";
+import r from "../../../responsive.module.css";
+import s from "./performance.module.css";
+import Link from "next/link";
+import axios from "axios";
+
+export default function Home() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const [isFull, setFull] = useState(false); /* Expands the body cont */
+  const [isCentered, setCentered] = useState(false); /* Centers the body cont */
+  const [isBottom, setBottom] = useState(true);
+  const trueCenter = false;
+
+  const [active, setActive] = useState(true);
+
+  /* CURRENT USER */
+  const [user, setUser] = useState<{
+    id: number;
+    email: string;
+    name: string;
+    team_id: number;
+    role: string;
+  }>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedUserString = localStorage.getItem("user");
+      setUser(JSON.parse(storedUserString || "[]"));
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (typeof window !== "undefined") {
+          const storedUser = localStorage.getItem("user");
+          if (storedUser) {
+            const user = JSON.parse(storedUser);
+            const teamID = user.team_id;
+
+            const response = await axios.get(
+              `https://team12.kenscourses.com/tasks/team/${teamID}`
+            );
+            const data = response.data;
+
+            // Set tasks data
+            setTasks(data);
+            console.log("Data fetched:", data);
+
+            // Save tasks data to local storage
+            localStorage.setItem("tasks", JSON.stringify(data));
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div
+      /* Main Container */
+      className={
+        isCentered
+          ? trueCenter
+            ? `${r.telegramHeight} ${r.trueCenter}`
+            : `${r.telegramHeight} ${r.centered}`
+          : `${r.telegramHeight} ${r.top}`
+      }
+    >
+      <script src="https://telegram.org/js/telegram-web-app.js"></script>
+
+      <div /* Top Wrapper */ className={`${r.wrapper} ${s.titleFlex}`}>
+        <div className={`${s.topTitle} font-bold`}>Performance Review</div>
+        <div className={` text-gray-600 `}>#Team {user?.team_id}</div>
+      </div>
+      <div className={s.sFont}>{user?.role}</div>
+
+      <div
+        /* Main Body */ className={
+          isFull ? `${r.body} ${r.full}` : `${r.body} ${r.fit}`
+        }
+      >
+        <div className={s.mainBody}>
+          <div /* Aqui Los Mapeamos*/>
+            <div className={s.standup}>
+              <div>uwu</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div
+        /* Bottom Wrapper */
+        className={isBottom ? `${r.wrapper} ${r.bottom}` : r.wrapper}
+      ></div>
+    </div>
+  );
+}
